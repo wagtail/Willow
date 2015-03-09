7f473b5e2c287b7ef858100049d2f3373016a6a4
Karl Hobley <karlhobley10@gmail.com>
2015-03-09 14:35:35 +0000

    Update transform.py

--- willow/transform.py
+++ willow/transform.py
@@ -2,13 +2,20 @@ from __future__ import division
 
 
 class Transform(object):
-    def __init__(self, width, height, matrix=None):
+    def __init__(self, width, height, scale_x=1.0, scale_y=1.0, offset_x=0.0, offset_y=0.0):
         self._width = width
         self._height = height
-        self._matrix = matrix or [1.0, 0.0, 0.0, 1.0, 0.0, 0.0]
+        self._scale_x = scale_x
+        self._scale_y = scale_y
+        self._offset_x = offset_x
+        self._offset_y = offset_y
 
     def _clone(self):
-        return type(self)(self._width, self._height, list(self._matrix))
+        return type(self)(
+            self._width, self._height,
+            self._scale_x, self._scale_y,
+            self._offset_x, self._offset_y
+        )
 
     def resize(self, width, height):
         new = self._clone()
@@ -36,18 +43,20 @@ class Transform(object):
         return self._width, self._height
 
     def get_matrix(self):
-        return self._matrix
+        return [
+            self._scale_x, 0,
+            0, self._scale_y,
+            self._offset_x, self._offset_y,
+        ]
 
     def _apply_size(self, width, height):
         self._width = width
         self._height = height
 
     def _apply_scale(self, x, y):
-        self._matrix[0] *= x
-        self._matrix[1] *= x
-        self._matrix[2] *= y
-        self._matrix[3] *= y
+        self._scale_x *= x
+        self._scale_y *= y
 
     def _apply_offset(self, x, y):
-        self._matrix[4] += self._matrix[0] * x + self._matrix[2] * y
-        self._matrix[5] += self._matrix[1] * x + self._matrix[3] * y
+        self._offset_x += self._scale_x * x
+        self._offset_y += x + self._scale_y * y
