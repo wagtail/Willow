970ae2bb667a2e9aeba7aab19d06a59e0038aecc
Karl Hobley <karlhobley10@gmail.com>
2015-05-10 14:19:11 +0100

    Update Pillow tests

--- tests/test_pillow.py
+++ tests/test_pillow.py
@@ -2,13 +2,14 @@ import unittest
 import io
 import imghdr
 
-from willow.backends import pillow as pillow_backend
+from willow.states.files import PNGImageFileState, GIFImageFileState
+from willow.states.pillow import PillowImageState
 
 
 class TestPillowOperations(unittest.TestCase):
     def setUp(self):
         with open('tests/images/transparent.png', 'rb') as f:
-            self.image = pillow_backend.PillowBackend.from_file(f)
+            self.image = PillowImageState.open(PNGImageFileState(f))
 
     def test_get_size(self):
         width, height = self.image.get_size()
@@ -16,12 +17,12 @@ class TestPillowOperations(unittest.TestCase):
         self.assertEqual(height, 150)
 
     def test_resize(self):
-        self.image.resize((100, 75))
-        self.assertEqual(self.image.image.size, (100, 75))
+        resized_image = self.image.resize((100, 75))
+        self.assertEqual(resized_image.get_size(), (100, 75))
 
     def test_crop(self):
-        self.image.crop((10, 10, 100, 100))
-        self.assertEqual(self.image.image.size, (90, 90))
+        cropped_image = self.image.crop((10, 10, 100, 100))
+        self.assertEqual(cropped_image.get_size(), (90, 90))
 
     def test_save_as_jpeg(self):
         output = io.BytesIO()
@@ -54,7 +55,7 @@ class TestPillowOperations(unittest.TestCase):
 
     def test_transparent_gif(self):
         with open('tests/images/transparent.gif', 'rb') as f:
-            image = pillow_backend.PillowBackend.from_file(f)
+            image = PillowImageState.open(GIFImageFileState(f))
 
         self.assertTrue(image.has_alpha())
         self.assertFalse(image.has_animation())
@@ -65,19 +66,19 @@ class TestPillowOperations(unittest.TestCase):
     @unittest.expectedFailure
     def test_resize_transparent_gif(self):
         with open('tests/images/transparent.gif', 'rb') as f:
-            image = pillow_backend.PillowBackend.from_file(f)
+            image = PillowImageState.open(GIFImageFileState(f))
 
-        image.resize((100, 75))
+        resized_image = image.resize((100, 75))
 
-        self.assertTrue(image.has_alpha())
-        self.assertFalse(image.has_animation())
+        self.assertTrue(resized_image.has_alpha())
+        self.assertFalse(resized_image.has_animation())
 
         # Check that the alpha of pixel 1,1 is 0
-        self.assertEqual(image.image.convert('RGBA').getpixel((1, 1))[3], 0)
+        self.assertEqual(resized_image.image.convert('RGBA').getpixel((1, 1))[3], 0)
 
     def test_save_transparent_gif(self):
         with open('tests/images/transparent.gif', 'rb') as f:
-            image = pillow_backend.PillowBackend.from_file(f)
+            image = PillowImageState.open(GIFImageFileState(f))
 
         # Save it into memory
         f = io.BytesIO()
@@ -85,7 +86,7 @@ class TestPillowOperations(unittest.TestCase):
 
         # Reload it
         f.seek(0)
-        image = pillow_backend.PillowBackend.from_file(f)
+        image = PillowImageState.open(GIFImageFileState(f))
 
         self.assertTrue(image.has_alpha())
         self.assertFalse(image.has_animation())
@@ -96,7 +97,7 @@ class TestPillowOperations(unittest.TestCase):
     @unittest.expectedFailure # Pillow doesn't support animation
     def test_animated_gif(self):
         with open('tests/images/newtons_cradle.gif', 'rb') as f:
-            image = pillow_backend.PillowBackend.from_file(f)
+            image = PillowImageState.open(GIFImageFileState(f))
 
         self.assertFalse(image.has_alpha())
         self.assertTrue(image.has_animation())
@@ -104,9 +105,9 @@ class TestPillowOperations(unittest.TestCase):
     @unittest.expectedFailure # Pillow doesn't support animation
     def test_resize_animated_gif(self):
         with open('tests/images/newtons_cradle.gif', 'rb') as f:
-            image = pillow_backend.PillowBackend.from_file(f)
+            image = PillowImageState.open(GIFImageFileState(f))
 
-        image.resize((100, 75))
+        resized_image = image.resize((100, 75))
 
-        self.assertFalse(image.has_alpha())
-        self.assertTrue(image.has_animation())
+        self.assertFalse(resized_image.has_alpha())
+        self.assertTrue(resized_image.has_animation())
