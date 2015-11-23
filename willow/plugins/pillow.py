1302e46fc03b087510f576e61e913c832e6363cb
Karl Hobley <karlhobley10@gmail.com>
2015-11-23 14:47:57 +0000

    Let Pillow load BMP files

--- willow/plugins/pillow.py
+++ willow/plugins/pillow.py
@@ -5,6 +5,7 @@ from willow.image import (
     JPEGImageFile,
     PNGImageFile,
     GIFImageFile,
+    BMPImageFile,
     RGBImageBuffer,
     RGBAImageBuffer,
 )
@@ -88,6 +89,7 @@ class PillowImage(Image):
     @Image.converter_from(JPEGImageFile)
     @Image.converter_from(PNGImageFile)
     @Image.converter_from(GIFImageFile, cost=200)
+    @Image.converter_from(BMPImageFile)
     def open(cls, image_file):
         image_file.f.seek(0)
         image = _PIL_Image().open(image_file.f)
