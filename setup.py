c37e9dc0c8c1cfb67bfedb5ccca7f5e4dfe566eb
Karl Hobley <karlhobley10@gmail.com>
2016-03-09 16:07:46 +0000

    Prepare for 0.3 release

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 setup(
     name='Willow',
-    version='0.3b4',
+    version='0.3',
     description='A Python image library that sits on top of Pillow, Wand and OpenCV',
     author='Karl Hobley',
     author_email='karlhobley10@gmail.com',
