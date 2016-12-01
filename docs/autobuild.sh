2e4b6ba706d9d1a54fe615b7dab5633f6077e2f5
Karl Hobley <karl@torchbox.com>
2016-12-01 09:40:59 +0000

    Deleted sphinx docs

--- docs/autobuild.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Waiting for you to save the docs..."
-watchmedo shell-command --patterns="*.rst" --ignore-pattern='_build/*' --recursive --command='make html; echo "Waiting for more changes..."'
