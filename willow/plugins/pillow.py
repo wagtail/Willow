a4573805afaff5c51bc0dd8cff3bf6b7277a63e4
Ștefan Istrate <stefan.istrate@gmail.com>
2018-02-19 18:25:17 +0000

    Keep existing ICC profile when images are saved as JPEG or PNG.

--- willow/plugins/pillow.py
+++ willow/plugins/pillow.py
@@ -98,6 +98,7 @@ class PillowImage(Image):
             kwargs['optimize'] = True
         if progressive:
             kwargs['progressive'] = True
+        kwargs['icc_profile'] = image.info.get('icc_profile')
 
         image.save(f, 'JPEG', quality=quality, **kwargs)
         return JPEGImageFile(f)
@@ -108,6 +109,7 @@ class PillowImage(Image):
         kwargs = {}
         if optimize:
             kwargs['optimize'] = True
+        kwargs['icc_profile'] = image.info.get('icc_profile')
 
         self.image.save(f, 'PNG', **kwargs)
         return PNGImageFile(f)
