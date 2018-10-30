8b16ece44eb41c295369deccfe6ba810432fbe52
Dan Braghis <dan.braghis@torchbox.com>
2018-10-30 10:01:46 +0000

    Add default optimizers

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -1,6 +1,7 @@
 from subprocess import STDOUT, CalledProcessError
 from unittest import TestCase, mock
 
+from willow.optimizers import Gifsicle, Jpegoptim, Optipng, Pngquant
 from willow.optimizers.base import OptimizerBase
 from willow.registry import WillowRegistry
 
@@ -34,13 +35,64 @@ class OptimizerTest(TestCase):
         self.assertTrue(self.DummyOptimizer.applies_to("FOO"))
         self.assertFalse(self.DummyOptimizer.applies_to("JPEG"))
 
+    def test_applies_to_for_default_optimizers(self):
+        self.assertTrue(Gifsicle.applies_to("gif"))
+        self.assertFalse(Gifsicle.applies_to("png"))
+        self.assertFalse(Gifsicle.applies_to("txt"))
+        self.assertTrue(Jpegoptim.applies_to("jpeg"))
+        self.assertFalse(Jpegoptim.applies_to("png"))
+        self.assertTrue(Optipng.applies_to("png"))
+        self.assertFalse(Optipng.applies_to("gif"))
+        self.assertTrue(Pngquant.applies_to("png"))
+        self.assertFalse(Pngquant.applies_to("jpeg"))
+
     def test_get_command_arguments(self):
         self.assertEqual(self.DummyOptimizer.get_command_arguments("file.png"), [])
+
+    def test_get_command_arguments_for_default_optimizers(self):
+        self.assertListEqual(
+            Gifsicle.get_command_arguments("file.gif"), ["-b", "-O3", "file.gif"]
+        )
+        self.assertListEqual(
+            Jpegoptim.get_command_arguments("file.jpg"),
+            ["--strip-all", "--max=85", "--all-progressive", "file.jpg"],
+        )
+        self.assertListEqual(
+            Optipng.get_command_arguments("file.png"),
+            ["-quiet", "-o2", "-i0", "file.png"],
+        )
+        self.assertListEqual(
+            Pngquant.get_command_arguments("file.png"),
+            ["--force", "--skip-if-larger", "file.png", "--output", "file.png"],
+        )
 
     @mock.patch("willow.optimizers.base.subprocess.check_output")
     def test_process(self, mock_check_output):
         self.DummyOptimizer.process("file.png")
         mock_check_output.assert_called_once_with(["dummy"], stderr=STDOUT)
+
+    @mock.patch("willow.optimizers.base.subprocess.check_output")
+    def test_process_for_default_optimizers(self, mock_check_output):
+        Gifsicle.process("file.gif")
+        mock_check_output.called_with(["gifsicle", "-b", "-O3", "file.gif"])
+
+        Jpegoptim.process("file.jpg")
+        mock_check_output.called_with("jpegoptim", "--strip-all", "file.jpg")
+
+        Optipng.process("file.png")
+        mock_check_output.called_with(["optipng", "-quiet", "-o2", "-i0", "file.png"])
+
+        Pngquant.process("file.png")
+        mock_check_output.called_with(
+            [
+                "pngquant",
+                "--force",
+                "--skip-if-larger",
+                "file.png",
+                "--output",
+                "file.png",
+            ]
+        )
 
     @mock.patch("willow.optimizers.base.subprocess.check_output")
     def test_process_logs_any_issue(self, mock_check_output):
