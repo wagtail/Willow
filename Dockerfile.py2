2e47b0ea36e545411ad2ecd9ac36bb5228eab4e5
Arnar Tumi Þorsteinsson <arnartumi@gmail.com>
2022-06-16 11:39:53 +0000

    remove some older python versions

--- Dockerfile.py2
+++
@@ -1,7 +0,0 @@
-FROM debian:jessie
-MAINTAINER Karl Hobley <karlhobley10@gmail.com>
-
-RUN apt-get update && apt-get install -y python python-opencv python-numpy python-pillow python-wand python-mock
-
-VOLUME ["/src"]
-WORKDIR /src
--- docker-test.sh
+++ docker-test.sh
@@ -1,8 +1,5 @@
 # Runs the tests using the provided Dockerfile
 # Saves having to install OpenCV locally
 
-docker build -f Dockerfile.py2 -t willow-py2 .
-docker run --rm -ti -v $(pwd):/src willow-py2 python runtests.py --opencv
-
 docker build -f Dockerfile.py3 -t willow-py3 .
 docker run --rm -ti -v $(pwd):/src willow-py3 python3 runtests.py --opencv
