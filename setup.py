62dbfba953f6deff03e8c7312200a4f32549e0f8
Matt Westcott <matt@west.co.tt>
2023-03-29 18:18:39 +0100

    Test against Python 3.11

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
     ],
     python_requires='>=3.7',
     install_requires=[
